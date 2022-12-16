1dd8c03a8cdc25e64979eecd08c55ffc60078986
mspark <mspark@openerd.com>
2022-12-16 13:34:32 +0900

    이미지 미리보기 추가

--- pages/write.vue
+++ pages/write.vue
@@ -4,27 +4,48 @@
       href="https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css"
       rel="stylesheet"
     />
-
     <Tiptap v-model="content" :max-limit="280" />
-    <input type="file" multiple @change="handleFileChange" />
-    <button class="submit-button" @click="onSubmit">Submit</button>
+    <div class="buttons">
+      <input type="file" multiple accept="image/*" @change="handleFileChange" />
+      <button class="submit-button" @click.prevent="onSubmit">Submit</button>
+    </div>
+    <div v-if="prevImage.length" class="preview-imgs">
+      <img
+        v-for="(src, i) in prevImage"
+        :key="i"
+        :src="src"
+        alt="prev img"
+        class="prev-img"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import axios from "axios";
+import { FileData } from "@/store/file";
 
 const router = useRouter();
 
 const content = ref<string>("");
-const file = ref([]);
+const file = ref<File[]>([]);
 
 function handleFileChange(e: any): void {
   file.value = e.target.files;
 }
 
+const prevImage = computed(() => {
+  const imgArr: string[] = [];
+  for (let i = 0; i < file.value.length; i++) {
+    if (file.value[i].type.includes("image"))
+      imgArr.push(URL.createObjectURL(file.value[i]));
+  }
+  return imgArr;
+});
+
 async function onSubmit() {
   const formData = new FormData();
+
   for (let i = 0; i < file.value.length; i++) {
     formData.append("file", file.value[i]);
   }
@@ -43,7 +64,7 @@ async function onSubmit() {
 #app {
   font-family: Arial;
   font-size: 16px;
-  margin: 0 2rem;
+  margin: 0 1rem;
 
   .submit-button {
     border: 1px solid #333;
@@ -61,5 +82,19 @@ async function onSubmit() {
       color: #fff;
     }
   }
+}
+
+.buttons {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  input {
+  }
+  margin-top: 0.5rem;
+}
+
+.prev-img {
+  width: 8rem;
+  height: 8rem;
 }
 </style>
